edff4b48fddba1c426e6cb204fd02974c89ae7cc
benjamin-t-wilson <benjiimanwillson@gmail.com>
2020-06-09 20:04:55 -0700

    finished moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,24 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
+    # count total number of 0s and set a pointer
+    zero_count = arr.count(0)
+    curr_count = 0
 
-    pass
+    # provided our current count is less than the total number
+    # we will not hit the index not found error
+    while curr_count < zero_count:
+        # remove the 0 and place at the end
+        arr.pop(arr.index(0))
+        arr.append(0)
+        # progress counter
+        curr_count += 1
+
+    return arr
 
 
 if __name__ == '__main__':
