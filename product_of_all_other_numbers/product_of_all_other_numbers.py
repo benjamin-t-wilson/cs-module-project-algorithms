cca33f952a846072051fb5a35ea80bf40d76fde6
benjamin-t-wilson <benjiimanwillson@gmail.com>
2020-06-09 20:48:14 -0700

    forgot to comment

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -7,8 +7,10 @@ import math
 
 def product_of_all_other_numbers(arr):
     # Your code here
+    # this is for holding
     new_arr = []
 
+    # for every item return product of remaining array
     for i in range(len(arr)):
         if i == 0:
             new_arr.append(math.prod(arr[1:]))
@@ -22,6 +24,7 @@ if __name__ == '__main__':
     # Use the main function to test your implementation
     # arr = [1, 2, 3, 4, 5]
     # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    # I made a small one for testing
     arr = [1, 2, 3]
 
     print(
