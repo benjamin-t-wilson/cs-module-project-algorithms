2d5d7c8c0fc856ba477574bf3219a1ac3e7c6439
benjamin-t-wilson <benjiimanwillson@gmail.com>
2020-06-09 20:52:15 -0700

    finished eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,32 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
 
-    pass
+# added a cache for recursiveness
+def eating_cookies(n, cache={}):
+    print(n, cache)
+    # check cache for key of n
+    if n in cache:
+        return cache[n]
+    
+    # handle 0 and 1 being passed
+    elif n < 0:
+        cache[n] = 0
+        return cache[n]
+    elif n == 0:
+        cache[n] = 1
+        return cache[n]
+    
+    # else, set the key to the recursive result of the func
+    else:
+        cache[n] = eating_cookies(
+            n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+        return cache[n]
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
